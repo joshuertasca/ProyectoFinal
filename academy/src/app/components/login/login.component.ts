9b621d75adc9c578e005092a905d5c8276cd3f7f
Emmanuelcq <ecarrascalq@gmail.com>
2022-06-09 18:40:51 -0500

    formulario reactivos

--- academy/src/app/components/login/login.component.ts
+++ academy/src/app/components/login/login.component.ts
@@ -9,25 +9,42 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class LoginComponent implements OnInit {
 
-contactoForm: FormGroup;
+loginForm: FormGroup;
+modalForm: FormGroup;
 regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
 regexNumero = /^[0-9]+$/;
+regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
 
 constructor(private fb: FormBuilder) {
-    this.contactoForm=this.fb.group({
-      Nombre:["",Validators.required],
-      Edad:["", [Validators.required, Validators.pattern(this.regexNumero)]],
-      Celular:["",[Validators.required, Validators.pattern(this.regexNumero)]],
+    this.loginForm=this.fb.group({
       Email:["", [Validators.required, Validators.pattern(this.regexEmail)]],
+      Password:["",[Validators.required, Validators.pattern(this.regexPassword)]]
 
 
     })
+
+    this.modalForm=this.fb.group({
+      Nombre:["",Validators.required],
+      Edad:["", [Validators.required, Validators.pattern(this.regexNumero)]],
+      SetPassword:["",[Validators.required, Validators.pattern(this.regexPassword)]],
+      SetEmail:["", [Validators.required, Validators.pattern(this.regexEmail)]],
+
+    })
+
+
+
+
   }
+
+
+
+
+
 
   ngOnInit(): void {
   }
   alimentarContacto(){
-      console.log(this.contactoForm)
+      console.log(this.loginForm)
   }
 
 }
