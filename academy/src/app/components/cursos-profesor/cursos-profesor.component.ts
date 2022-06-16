6fd266ce7d04e2ea35ae5ad4c5716ba40b558936
joshuertasca <joshuertasca@unal.edu.co>
2022-06-16 02:10:19 -0500

    crear cursos

--- academy/src/app/components/cursos-profesor/cursos-profesor.component.ts
+++ academy/src/app/components/cursos-profesor/cursos-profesor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, Renderer2, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -8,49 +9,128 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosProfesorComponent implements OnInit {
 
-  cursos: any = [
-    {
-      "nombre": "biologia",
-      "contenido": [
-        {
-          "tipo": "texto",
-          "nombre": "ciclo del Agua",
-          "informacion": "El ciclo hidrológico comienza con la evaporación del agua desde la superficie del océano. A medida que se eleva, el aire humedecido se enfría y el vapor se transforma en agua: es la condensación. Las gotas se juntan y forman una nube. Luego, caen por su propio peso: es la precipitación."
-        },
-        {
-          "tipo": "imagen",
-          "nombre": "ciclo del Agua 2",
-          "informacion": "https://www.educapeques.com/wp-content/uploads/2016/05/ciclo-del-agua.jpg"
-        },
-        ,
-        {
-          "tipo": "video",
-          "nombre": "ciclo del Agua 3",
-          "informacion": "https://www.youtube.com/watch?v=ITauNE9YNdE"
-        }
-      ]
-    }
-  ]
+  @ViewChild('imagen') imagenHTML?: ElementRef
+  @ViewChild('boton') botonHTML?: ElementRef
+  @ViewChild('addimagen') addimagenHTML?: ElementRef
+  @ViewChild('imputdiapositiva') imputHTML?: ElementRef
+  @ViewChild('tabla') tablaHTML?: ElementRef
+  @ViewChild('botoncrearcurso') crearHTML?: ElementRef
+  @ViewChild('advertencia') advHTML?: ElementRef
+  @ViewChild('tituloDiapositiva') tituloHTML?: ElementRef
 
-  constructor() { }
+
+
+  cursos: any = []
+  tipo: any = "imagen";
+  contenidoCurso: any = [];
+  tabla: any = "";
+  contador: number = 1;
+  cursocreado: any = {};
+
+  constructor(private renderer2: Renderer2, private router: Router, private idRouter: ActivatedRoute) { };
+
 
   ngOnInit(): void {
   }
 
 
-  addImagen(){
+  addImagen(): void {
+    const imagen = this.addimagenHTML?.nativeElement;
+    const imput = this.imputHTML?.nativeElement;
+    this.renderer2.setProperty(imagen, 'innerHTML', 'Agregar Imagen');
+    this.renderer2.setAttribute(imput, 'placeholder', 'Pegar Link de la Imagen')
+    this.tipo = "imagen";
 
 
   }
-  addVideo(){
+  addVideo() {
+    const imagen = this.addimagenHTML?.nativeElement;
+    const imput = this.imputHTML?.nativeElement;
+    this.renderer2.setProperty(imagen, 'innerHTML', 'Agregar Video')
+    this.renderer2.setAttribute(imput, 'placeholder', 'Pegar Link del video')
+    this.tipo = "video";
 
 
   }
-  addTexto(){
+  addTexto() {
+    const imagen = this.addimagenHTML?.nativeElement;
+    const imput = this.imputHTML?.nativeElement;
 
+    this.renderer2.setProperty(imagen, 'innerHTML', 'Agregar Texto')
+    this.renderer2.setAttribute(imput, 'placeholder', 'Escribir Texto')
+
+    this.tipo = "texto";
 
   }
 
 
+  agregardiapositiva(titulo: string, conte: string, nombrecurso: string) {
+    const advertencia = this.advHTML?.nativeElement;
+    if (nombrecurso == '') {
+
+      this.renderer2.setProperty(advertencia, 'innerHTML', 'Antes de agregar contenido debe agregar un nombre al curso')
+    } else {
+
+      this.renderer2.setProperty(advertencia, 'innerHTML', '')
+
+      if (titulo == '' || conte == '') {
+        this.renderer2.setProperty(advertencia, 'innerHTML', 'No puede agregar un ' + this.tipo + ' sin completar ambos campos')
+      } else {
+        let objeto: any = {
+          "tipo": this.tipo,
+          "nombre": titulo,
+          "informacion": conte
+        }
+        this.contenidoCurso.push(objeto)
+        this.tabla = this.tabla + ' <tr> <th scope="row">' + this.contador + '</th>  <td>' + this.tipo + '</td>  <td>' + titulo + '</td> </tr> '
+        this.contador++
+
+        const tabla = this.tablaHTML?.nativeElement;
+        this.renderer2.setProperty(tabla, 'innerHTML', this.tabla);
+        const botoncrear = this.crearHTML?.nativeElement;
+        this.renderer2.removeAttribute(botoncrear, 'disabled');
+
+
+      }
+
+
+
+    }
+
+    const titulodi = this.tituloHTML?.nativeElement;   //preguntar como borrar los campos
+    this.renderer2.setAttribute(titulodi, 'value', '');
+
+  }
+
+
+  crearCurso(nombre: string) {
+
+    this.cursocreado = {
+      "nombre": nombre,
+      "contenido": this.contenidoCurso
+    }
+
+    this.cursos.push(this.cursocreado)
+    console.log(this.cursos)
+    this.tipo = "imagen";
+    this.contenidoCurso = [];
+    this.tabla = "";
+    this.contador = 1;
+    const tabla = this.tablaHTML?.nativeElement;
+    this.renderer2.setProperty(tabla, 'innerHTML', this.tabla);
+
+    const botoncrear = this.crearHTML?.nativeElement;
+    this.renderer2.setAttribute(botoncrear, 'disabled', '');
+
+  }
+
+  abrirCurso(posicion: number) {
+    console.log(posicion) // es el numero de la posicion del curso
+  }
+
+  eliminarCurso(posicion: number) {
+    this.cursos.splice(posicion, 1)
+    console.log(this.cursos)
+  }
 
 }
